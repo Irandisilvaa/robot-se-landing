d9ff3403733728527c3d7a9e23b29bdb29856006
Irandi Silva <167270911+Irandisilvaa@users.noreply.github.com>
2026-06-22 19:23:37 -0300

    Atualizar o tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,27 @@ export default {
   theme: {
     extend: {
       colors: {
-        robotNavy: "#071A2F",
-        robotYellow: "#FFD500",
-        robotGreen: "#00C896",
+        robotNavy: "#050816",
+        robotBlue: "#00D9FF",
+        robotPurple: "#7C3AED",
+        robotYellow: "#FFE600",
+        robotGreen: "#00FFAE",
       },
+
+      boxShadow: {
+        neon: "0 0 25px rgba(0,217,255,0.35)",
+      },
+
+      backgroundImage: {
+        grid: `
+          linear-gradient(rgba(255,255,255,0.04) 1px, transparent 1px),
+          linear-gradient(90deg, rgba(255,255,255,0.04) 1px, transparent 1px)
+        `,
+      },
+
+      backgroundSize: {
+        grid: "40px 40px",
+      }
     },
   },
   plugins: [],
